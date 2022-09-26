1d29e8b0191f048ee0c2635b213c7f17b812db76
Henry Hoegelow <henry@hoegelow.com>
2022-09-26 18:33:38 +0200

    WIP

--- cmake/podman.cmake
+++ cmake/podman.cmake
@@ -1,19 +1,17 @@
-function(registerPod DOCKERFILE)
-  configure_file(${DOCKERFILE} ${CMAKE_CURRENT_BINARY_DIR}/Dockerfile @ONLY)
+function(registerPod)
+  configure_file(Dockerfile.in ${CMAKE_CURRENT_BINARY_DIR}/Dockerfile @ONLY)
   file(SHA1 ${CMAKE_CURRENT_BINARY_DIR}/Dockerfile DOCKERFILE_SHA1)
   
   set(DOCKERNAME ${BUILD_TASK}-${TARGET_MACHINE})
+
+  configure_file(${CMAKE_SOURCE_DIR}/cmake/runPodman.sh.in ${CMAKE_CURRENT_BINARY_DIR}/runPodman.sh)
   
   add_custom_command(
     COMMENT "Provide Podman for ${DOCKERNAME}"
     OUTPUT .podman
     DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/Dockerfile
     VERBATIM
-    COMMAND
-      podman run --tls-verify=false --rm ${PODMAN_REGISTRY}/${DOCKERNAME}:${DOCKERFILE_SHA1} 
-      || podman build -t ${DOCKERNAME}:${DOCKERFILE_SHA1} .
-        && podman push --tls-verify=false ${DOCKERNAME}:${DOCKERFILE_SHA1} ${PODMAN_REGISTRY}/${DOCKERNAME}:${DOCKERFILE_SHA1}
-    )
+    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/runPodman.sh)
   
   message("Add pod ${DOCKERNAME}")
   add_custom_target(${DOCKERNAME} DEPENDS .podman)
@@ -28,39 +26,40 @@ function(crossBuild NAME)
 
   # commands for cross building all the desired packages
   foreach(PACKAGE ${ARGN})
-    STRING(TOUPPER ${PACKAGE} PACKAGE_CANONIC)
-    STRING(REPLACE "-" "_" PACKAGE_CANONIC ${PACKAGE_CANONIC})
+    string(TOUPPER ${PACKAGE} PACKAGE_CANONIC)
+    string(REPLACE "-" "_" PACKAGE_CANONIC ${PACKAGE_CANONIC})
+    set(POD podman run 
+      --tls-verify=false --rm 
+      -v ${CMAKE_SOURCE_DIR}/packages/${PACKAGE}:/src 
+      -v ${CMAKE_CURRENT_BINARY_DIR}/build-${PACKAGE}:/build 
+      -w /build
+      ${PODMAN_REGISTRY}/${DOCKERNAME}:${DOCKERFILE_SHA1})
+
+    file(GLOB_RECURSE ALL_PROJECT_FILES ${CMAKE_SOURCE_DIR}/packages/${PACKAGE}/*)
+
     add_custom_command(
       COMMENT "X-Building ${PACKAGE} for ${TARGET_MACHINE}"
       OUTPUT ${PACKAGE}.deb
+      DEPENDS ${ALL_PROJECT_FILES}
       VERBATIM
       COMMAND mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/build-${PACKAGE}
-      COMMAND podman run --tls-verify=false --rm 
-        -v ${CMAKE_SOURCE_DIR}/packages/${PACKAGE}:/src
-        -v ${CMAKE_CURRENT_BINARY_DIR}/build-${PACKAGE}:/build
-        ${PODMAN_REGISTRY}/${DOCKERNAME}:${DOCKERFILE_SHA1} cmake -S /src -B /build
-      COMMAND podman run --tls-verify=false --rm 
-        -v ${CMAKE_SOURCE_DIR}/packages/${PACKAGE}:/src 
-        -v ${CMAKE_CURRENT_BINARY_DIR}/build-${PACKAGE}:/build
-        ${PODMAN_REGISTRY}/${DOCKERNAME}:${DOCKERFILE_SHA1} make -C /build 
-      COMMAND podman run --tls-verify=false --rm 
-        -v ${CMAKE_SOURCE_DIR}/packages/${PACKAGE}:/src
-        -v ${CMAKE_CURRENT_BINARY_DIR}/build-${PACKAGE}:/build
-        -w /build
-        ${PODMAN_REGISTRY}/${DOCKERNAME}:${DOCKERFILE_SHA1} cpack
-     )
+      COMMAND ${POD} cmake -S /src -B /build
+      COMMAND ${POD} make -C /build install
+      COMMAND ${POD} cpack -D CPACK_PACKAGE_FILE_NAME=${PACKAGE}
+      COMMAND cp ${CMAKE_CURRENT_BINARY_DIR}/build-${PACKAGE}/${PACKAGE}.deb ${CMAKE_CURRENT_BINARY_DIR})
   endforeach()
 
   SET(DEPENDENCIES ${ARGN})
-  list(TRANSFORM DEPENDENCIES PREPEND ${CMAKE_CURRENT_BINARY_DIR}/)
   list(TRANSFORM DEPENDENCIES APPEND .deb)
+  SET(DEPENDENCIES_FILES ${DEPENDENCIES})
+  list(TRANSFORM DEPENDENCIES PREPEND ${CMAKE_CURRENT_BINARY_DIR}/)
 
   add_custom_command(
     COMMENT Build ${NAME}
     DEPENDS ${DEPENDENCIES}
     OUTPUT ${NAME}.tar.gz
     COMMAND echo "pack the deps into a tar gz"
-    COMMAND tar -czf ${NAME}.tar.gz ${DEPENDENCIES}
+    COMMAND tar -czf ${NAME}.tar.gz -C ${CMAKE_CURRENT_BINARY_DIR} ${DEPENDENCIES_FILES}
   )
 
   add_custom_target(${NAME} DEPENDS ${NAME}.tar.gz)
